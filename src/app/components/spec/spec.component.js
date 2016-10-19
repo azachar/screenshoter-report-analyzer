b10f3952ee551926209f84c66fdd36ca31a7c938
Andrej Zachar <andrej@chocolatejar.eu>
2016-10-19 16:29:14 +0200

    fix(refactor): show spec screenshots
    resolve https://github.com/azachar/protractor-screenshoter-plugin/issues/3

--- src/app/components/spec/spec.component.js
+++ src/app/components/spec/spec.component.js
@@ -1,21 +1,21 @@
 class SpecController {
-  constructor($scope, $log) {
+  constructor($scope) {
     'ngInject';
     this.$onInit = () => {
       $scope.$watch(() => this.filtering.expand, (value) => {
         this.expand = value;
       });
-      $scope.$watch(() => this.filtering.expand, (value) => {
-        this.expand = value;
-      });
+
       $scope.$watch(() => this.filtering.showFailed, (value) => {
         this.showFailed = value;
-        $log.debug('showFailed = ', value);
       });
 
       $scope.$watch(() => this.filtering.showPassed, (value) => {
         this.showPassed = value;
-        $log.debug('showPassed = ', value);
+      });
+
+      $scope.$watch(() => this.filtering.showSpecScreenshots, (value) => {
+        this.showSpecScreenshots = value;
       });
     }
   }
